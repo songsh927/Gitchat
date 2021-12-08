8c3dc59bacb6faf357525b5f9228cf877ba9068d
songsh927 <songsh96927@gmail.com>
2021-12-08 12:19:37 +0900

    ver1.3.2

--- server/controller/chat.js
+++ server/controller/chat.js
@@ -1,29 +1,40 @@
 import {Server} from 'socket.io'
-import ios from 'express-socket.io-session'
-import session from 'express-session';
-import * as userRepo from '../data/auth.js'
+import cookie from 'cookie'
+import fs from 'fs'
+import cookieParser from 'cookie-parser';
+import {config} from '../config.js'
 
 export function initSocket(server){
     const io = new Server(server);
-
-    io.on('connection', (socket) => {
-        socket.on('chat message', (msg) => {
-            io.emit('chat message', msg);
+        io.on('connection', (socket) => {
+            socket.on('chat message', (msg) => {
+                const cookieStr = socket.handshake.headers.cookie;
+                if (cookieStr) {
+                    const cookieParsed = cookie.parse(cookieStr);
+                    const strArray = Object.values(cookieParsed)[0].split(':');
+                    const sidArray = strArray[1].split('.');
+    
+                    if(sidArray[0]){
+                        const sidParsed = cookieParser.signedCookie(sidArray[0], config.session.secretKey);
+                        fs.stat('./sessions/'+sidParsed + '.json' , (err,stats) => {
+                        if(err){
+                            console.log("cannot find file")
+                        }
+                        fs.readFile('./sessions/' + sidParsed + '.json','utf-8', (err, data) =>{
+                            if(err) throw err;
+                            const uidIndex = data.split('"').indexOf('userName');
+                            console.log(uidIndex);
+                            const uid = data.split('"')[uidIndex+2];
+                            console.log(uid);
+                            io.emit('chat message',uid+': '+ msg);
+                        });
+                    });
+                }
+            }
         });
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
         });
     });
+    
 }
-
-
-/*
-/main\chat
-{
-	userid,
-	username,
-	token,
-	talk_seq_num,
-	createTime
-}
-*/
